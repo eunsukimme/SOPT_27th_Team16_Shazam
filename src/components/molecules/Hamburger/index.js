7f96924359d9084dfec7c5f2daa6dd649bfd1311
donghunee <ehdgns1766@naver.com>
2020-12-05 15:20:02 +0900

    hambergur

--- src/components/molecules/Hamburger/index.js
+++ src/components/molecules/Hamburger/index.js
@@ -1,5 +1,6 @@
 import LogoIcon from '@images/logo_dark.svg';
 import propTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import Styled from 'styled-components';
 
 const HamburgerWrap = Styled.div`
@@ -14,6 +15,7 @@ const HamburgerWrap = Styled.div`
   right: 0;
   height: 100vh;
   width: 100%;
+  max-width: 400px;
   transition: transform 0.3s ease-in-out;
 
 `;
@@ -21,13 +23,81 @@ const HamburgerWrap = Styled.div`
 const ImageIcon = Styled.img`
   width: 99px;
   height: 28px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
 `;
 
-function Hamburger({ open, setOpen }) {
+const CancelIcon = Styled.div`
+  width: 20px;
+  height: 20px;
+  color: white;
+  position: absolute;
+  top: 20px; 
+  right: 20px;
+  cursor:pointer;
+`;
+
+const ListWrap = Styled.div`
+  margin-top: 100px;
+  padding: 40px;
+`;
+
+const ListTitle = Styled.div`
+  color:white;
+  font-weight: bolder;
+  font-size: 20px;
+  &+& {
+    margin-top: 53px;
+  }
+  cursor:pointer;
+`;
+
+function Hamburger({ open, setOpen, history }) {
   return (
-    <HamburgerWrap onClick={() => setOpen(!open)} open={open}>
+    <HamburgerWrap open={open}>
       <ImageIcon src={LogoIcon} />
-      <div setOpen={setOpen}></div>
+      <CancelIcon onClick={() => setOpen(!open)} open={open}>
+        x
+      </CancelIcon>
+      <ListWrap>
+        <ListTitle
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          Download Shazam
+        </ListTitle>
+        <ListTitle
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          app
+        </ListTitle>
+        <ListTitle
+          onClick={() => {
+            history.push('/list/0');
+            setOpen(!open);
+          }}
+        >
+          차트
+        </ListTitle>
+        <ListTitle
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          내 라이브러리
+        </ListTitle>
+        <ListTitle
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          도움말
+        </ListTitle>
+      </ListWrap>
     </HamburgerWrap>
   );
 }
@@ -35,6 +105,7 @@ function Hamburger({ open, setOpen }) {
 Hamburger.propTypes = {
   open: propTypes.bool,
   setOpen: propTypes.object,
+  history: propTypes.object,
 };
 
-export default Hamburger;
+export default withRouter(Hamburger);
